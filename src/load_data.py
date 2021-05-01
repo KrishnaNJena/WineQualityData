9627959969a744fa1d9c42f01048c49061a77367
KrishnaNJena <krishnaisb4u@gmail.com>
2021-05-01 08:39:48 +0530

    Stage 1 Complete

--- src/load_data.py
+++ src/load_data.py
@@ -1,8 +1,8 @@
 # read the data from data source
 # save it in the data/raw for further process
 import os
+from get_data import read_params, get_data
 import argparse
-from get_data import read_params,get_data
 
 def load_and_save(config_path):
     config = read_params(config_path)
